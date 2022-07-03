d6c94e853d1ecbf6a84362c120bd4d7c96c846be
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-07-03 14:48:25 +0300

    3.07.22

---
+++ ConsoleApp1/Concrete/Kaplumbaga.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Concrete
+{
+    public class Kaplumbaga : Hayvan
+    {
+        public override void KosmaHizi()
+        {
+            Console.WriteLine("Kosma Hızı= 1km/h");
+        }
+    }
+}
---
+++ ConsoleApp1/Concrete/Kopek.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Concrete
+{
+    public class Kopek : Hayvan
+    {
+        public bool EvcilMi { get; set; }
+        public string Cinsi { get; set; }
+
+        public override void KosmaHizi()
+        {
+            Console.WriteLine("Koşma Hızı 15km/h");
+        }
+    }
+}
